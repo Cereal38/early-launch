3d521dda4f90fa0b737b95a1c30ed5f27a44f086
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-07-07 09:58:10 +0200

    First endpoints in server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 "use strict";
-const app = require("express")();
+const express = require("express");
+const app = express();
 const PORT = 8080;
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.status(200).send({ message: "Hello There!" });
+});
+app.post("/user/:id", (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    if (!name) {
+        res.status(418).send({ message: "No name provided" });
+    }
+    res.status(200).send({ message: `${id} : ${name}` });
+});
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));

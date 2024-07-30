c4fd8e64d41659d6afae6983acb78ebdcfc54cde
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-07-30 15:38:49 +0200

    Init a custom button

---
+++ client/src/app/lib/components/custom-button/custom-button.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      custom-button works!
+    </p>
+  `,
+  styleUrl: './custom-button.component.css'
+})
+export class CustomButtonComponent {
+
+}

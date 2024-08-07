4e0b663435a2ee6e6d07437fb49c653456ab7e68
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-07 16:48:02 +0200

    Call the test api endpoint from the button

--- client/src/app/lib/components/custom-button/custom-button.component.ts
+++ client/src/app/lib/components/custom-button/custom-button.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-custom-button',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './custom-button.component.html',
   styleUrl: './custom-button.component.css',
 })
-export class CustomButtonComponent {}
+export class CustomButtonComponent {
+  constructor(private apiService: ApiService) {}
+
+  onClick() {
+    this.apiService.getHello().subscribe((response) => {
+      console.log('TEST API FETCH', response);
+    });
+  }
+}

ddd4d9d6ac441f77e50cd9962a272e3fb2110414
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-19 12:26:48 +0200

    Init admin route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+];

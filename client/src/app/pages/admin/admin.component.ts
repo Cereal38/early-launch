a228edf94151a32cdeec8f8f8103193c15b98487
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-19 18:51:30 +0200

    Type emails by creating an interface

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Email } from '../../lib/interfaces/email.interface';
 import { ApiService } from '../../lib/services/api.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { ApiService } from '../../lib/services/api.service';
   styleUrl: './admin.component.css',
 })
 export class AdminComponent {
-  emails: any = [];
+  emails: Email[] = [];
 
   constructor(private apiService: ApiService) {
     this.apiService.getEmails().subscribe((response) => {

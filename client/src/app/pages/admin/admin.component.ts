394ee1421b49a47ddaa5c58a963d36300abd359e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-20 17:43:39 +0200

    Pass email from the admin page to list component

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -2,11 +2,12 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { Email } from '../../lib/interfaces/email.interface';
 import { ApiService } from '../../lib/services/api.service';
+import { EmailListItemComponent } from './components/email-list-item/email-list-item.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, EmailListItemComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
--- client/src/app/pages/admin/components/email-list-item/email-list-item.component.ts
+++ client/src/app/pages/admin/components/email-list-item/email-list-item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Email } from '../../../../lib/interfaces/email.interface';
 
 @Component({
   selector: 'app-email-list-item',
   standalone: true,
   imports: [],
   templateUrl: './email-list-item.component.html',
-  styleUrl: './email-list-item.component.css'
+  styleUrl: './email-list-item.component.css',
 })
 export class EmailListItemComponent {
-
+  @Input() email!: Email;
 }

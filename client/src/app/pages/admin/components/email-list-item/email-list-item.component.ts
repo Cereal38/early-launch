21ab5cc83b826305f448b988f95179781f758284
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-20 17:36:09 +0200

    Init email-list-item

---
+++ client/src/app/pages/admin/components/email-list-item/email-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-list-item.component.html',
+  styleUrl: './email-list-item.component.css'
+})
+export class EmailListItemComponent {
+
+}

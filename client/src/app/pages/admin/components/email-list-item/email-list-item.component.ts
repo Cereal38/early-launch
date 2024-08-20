0e661a14436286d9652b0b1ad91d5efb29998238
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-20 17:56:54 +0200

    Handle email delete

--- client/src/app/pages/admin/components/email-list-item/email-list-item.component.ts
+++ client/src/app/pages/admin/components/email-list-item/email-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Email } from '../../../../lib/interfaces/email.interface';
+import { ApiService } from '../../../../lib/services/api.service';
 
 @Component({
   selector: 'app-email-list-item',
@@ -9,5 +10,13 @@ import { Email } from '../../../../lib/interfaces/email.interface';
   styleUrl: './email-list-item.component.css',
 })
 export class EmailListItemComponent {
+  constructor(private apiService: ApiService) {}
+
   @Input() email!: Email;
+
+  handleDelete() {
+    this.apiService.deleteEmail(this.email.id).subscribe((response) => {
+      console.log(`${this.email.address} deleted`);
+    });
+  }
 }

bdb21f14468c4390d9513d437e44751d5213d40c
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-25 17:44:55 +0200

    Add all spartan-ng components to the project

---
+++ client/src/app/lib/spartan-ng/ui-select-helm/src/lib/hlm-select-scroll-down.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { lucideChevronDown } from '@ng-icons/lucide';
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
+
+@Component({
+	selector: 'hlm-select-scroll-down',
+	standalone: true,
+	imports: [HlmIconComponent],
+	providers: [provideIcons({ lucideChevronDown })],
+	host: {
+		class: 'flex cursor-default items-center justify-center py-1',
+	},
+	template: `
+		<hlm-icon class="w-4 h-4 ml-2" name="lucideChevronDown" />
+	`,
+})
+export class HlmSelectScrollDownComponent {}

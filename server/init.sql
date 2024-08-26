f555c99b354558d25d3b3e32dbaa10de6fcb041c
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-26 13:17:12 +0200

    Create sample emails address in the server

---
+++ server/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS emails (id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT);
+
+INSERT INTO emails (address) VALUES ('sample1@example.com');
+INSERT INTO emails (address) VALUES ('sample2@example.com');
+INSERT INTO emails (address) VALUES ('sample3@example.com');

c12451006050cd2a1eb0aa0d2cfe6536a7577f48
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-26 21:54:50 +0200

    Init error message in form

--- client/src/app/pages/form/form.component.ts
+++ client/src/app/pages/form/form.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lucideAlertTriangle } from '@ng-icons/lucide';
+import { provideIcons } from '@spartan-ng/ui-icon-helm';
 import { CustomButtonComponent } from '../../lib/components/custom-button/custom-button.component';
 import { CustomTextFieldComponent } from '../../lib/components/custom-text-field/custom-text-field.component';
 import { ApiService } from '../../lib/services/api.service';
+import { HlmAlertIconDirective } from '../../lib/spartan-ng/ui-alert-helm/src/lib/hlm-alert-icon.directive';
+import { HlmAlertTitleDirective } from '../../lib/spartan-ng/ui-alert-helm/src/lib/hlm-alert-title.directive';
+import { HlmAlertDirective } from '../../lib/spartan-ng/ui-alert-helm/src/lib/hlm-alert.directive';
+import { HlmIconComponent } from '../../lib/spartan-ng/ui-icon-helm/src/lib/hlm-icon.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [RouterOutlet, CustomTextFieldComponent, CustomButtonComponent],
+  imports: [
+    RouterOutlet,
+    CustomTextFieldComponent,
+    CustomButtonComponent,
+    HlmAlertDirective,
+    HlmAlertDirective,
+    HlmAlertIconDirective,
+    HlmAlertTitleDirective,
+    HlmIconComponent,
+  ],
+  providers: [
+    provideIcons({
+      lucideAlertTriangle,
+    }),
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })

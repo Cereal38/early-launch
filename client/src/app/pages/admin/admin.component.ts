40ee850cd078988e0a81cb3acfff716bbd1a5cf4
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-26 13:56:03 +0200

    Use shadcn list component to display emails

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -2,12 +2,27 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { Email } from '../../lib/interfaces/email.interface';
 import { ApiService } from '../../lib/services/api.service';
+import { HlmCheckboxComponent } from '../../lib/spartan-ng/ui-checkbox-helm/src/lib/hlm-checkbox.component';
+import { HlmCaptionComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-caption.component';
+import { HlmTableComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-table.component';
+import { HlmTdComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-td.component';
+import { HlmThComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-th.component';
+import { HlmTrowComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-trow.component';
 import { EmailListItemComponent } from './components/email-list-item/email-list-item.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [NgFor, EmailListItemComponent],
+  imports: [
+    NgFor,
+    EmailListItemComponent,
+    HlmTableComponent,
+    HlmCaptionComponent,
+    HlmTrowComponent,
+    HlmThComponent,
+    HlmTdComponent,
+    HlmCheckboxComponent,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })

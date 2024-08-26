e4f6050403460c5f335b56464b729f125360c83f
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-26 13:19:42 +0200

    Update init.sql script

--- server/init.sql
+++ server/init.sql
@@ -1,5 +1,17 @@
 CREATE TABLE IF NOT EXISTS emails (id INTEGER PRIMARY KEY AUTOINCREMENT, address TEXT);
 
-INSERT INTO emails (address) VALUES ('sample1@example.com');
-INSERT INTO emails (address) VALUES ('sample2@example.com');
-INSERT INTO emails (address) VALUES ('sample3@example.com');
+INSERT INTO emails (address) VALUES ('Evaëlle@example.com');
+INSERT INTO emails (address) VALUES ('Zéphyr@example.com');
+INSERT INTO emails (address) VALUES ('Florin@example.com');
+INSERT INTO emails (address) VALUES ('Edelyne@example.com');
+INSERT INTO emails (address) VALUES ('Lilwenn@example.com');
+INSERT INTO emails (address) VALUES ('Macha@example.com');
+INSERT INTO emails (address) VALUES ('Madala@example.com');
+INSERT INTO emails (address) VALUES ('Malhone@example.com');
+INSERT INTO emails (address) VALUES ('Auxane@example.com');
+INSERT INTO emails (address) VALUES ('Elissandre@example.com');
+INSERT INTO emails (address) VALUES ('Manoé@example.com');
+INSERT INTO emails (address) VALUES ('Pakita@example.com');
+INSERT INTO emails (address) VALUES ('Sabaya@example.com');
+INSERT INTO emails (address) VALUES ('Tahissia@example.com');
+INSERT INTO emails (address) VALUES ('Taïla@example.com');

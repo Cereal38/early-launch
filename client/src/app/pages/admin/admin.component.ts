0986591537e2ef86a5e63a8e44215c25a2b33b9e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-08-26 16:26:33 +0200

    Add the delete button

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -1,7 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { lucideTrash2 } from '@ng-icons/lucide';
+import { provideIcons } from '@spartan-ng/ui-icon-helm';
 import { Email } from '../../lib/interfaces/email.interface';
 import { ApiService } from '../../lib/services/api.service';
+import { HlmButtonDirective } from '../../lib/spartan-ng/ui-button-helm/src/lib/hlm-button.directive';
+import { HlmIconComponent } from '../../lib/spartan-ng/ui-icon-helm/src/lib/hlm-icon.component';
 import { HlmCaptionComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-caption.component';
 import { HlmTableComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-table.component';
 import { HlmTdComponent } from '../../lib/spartan-ng/ui-table-helm/src/lib/hlm-td.component';
@@ -20,7 +24,10 @@ import { EmailListItemComponent } from './components/email-list-item/email-list-
     HlmTrowComponent,
     HlmThComponent,
     HlmTdComponent,
+    HlmButtonDirective,
+    HlmIconComponent,
   ],
+  providers: [provideIcons({ lucideTrash2 })],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
@@ -31,9 +38,7 @@ export class AdminComponent {
     this.apiService.getEmails().subscribe((response) => {
       // Format date
       const rawEmails = response;
-
       this.emails = rawEmails.map((email: Email) => {
-        // YYYY-MM-DD
         email.date = email.date.slice(0, 10);
         return email;
       });
